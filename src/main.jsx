5420fd9eecdb6006b6a510bab5907eee875b9198
hesoyamaezakmovich <ruhaha17@gmail.com>
2025-05-29 19:16:36 +0300

    ывываав

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import AppQueryProvider from './QueryClientProvider'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppQueryProvider>
-      <App />
-    </AppQueryProvider>
+    <App />
   </StrictMode>,
 )
