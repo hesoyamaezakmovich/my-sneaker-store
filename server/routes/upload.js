54d39db68e1257cf13f221ac0bcbe3f5fe3e9654
hesoyamaezakmovich <ruhaha17@gmail.com>
2026-05-19 01:04:02 +0300

    фывфывфвыццк

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -11,7 +11,9 @@ const s3 = new S3Client({
   endpoint: process.env.S3_ENDPOINT,
   region:   process.env.S3_REGION || 'ru-central-1',
   credentials: {
-    accessKeyId:     process.env.S3_ACCESS_KEY_ID,
+    accessKeyId:     process.env.S3_TENANT_ID
+      ? `${process.env.S3_TENANT_ID}:${process.env.S3_ACCESS_KEY_ID}`
+      : process.env.S3_ACCESS_KEY_ID,
     secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || process.env.S3_SECRET_KEY_ID,
   },
   forcePathStyle: true,
