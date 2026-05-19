1c70cfca54c96a3463ebb4ef5537cd3de00ff033
hesoyamaezakmovich <ruhaha17@gmail.com>
2026-05-19 12:47:47 +0300

    кцукцукцку

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(cors({
   credentials: true,
 }))
 app.use(express.json({ limit: '50mb' }))
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 
 // Статические файлы загрузок
 app.use('/api/uploads', express.static(path.join(__dirname, '../uploads')))
